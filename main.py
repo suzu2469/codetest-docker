7ffe5f19065eef13b92c5723c88b2519eb9417bf
shohei0203 <umegiwa@gmail.com>
2022-02-21 23:11:27 +0900

    fix response code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pymysql
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 
+import repository.transaction
 from dependency import get_db
-from transaction import Transaction
+from model.transaction import Transaction
 
 app = FastAPI()
 
@@ -12,6 +13,10 @@ async def root():
     return {"message": "code test"}
 
 
-@app.post("/transactions")
+@app.post("/transactions", status_code=status.HTTP_201_CREATED)
 async def transactions(t: Transaction, db: pymysql.Connection = Depends(get_db)):
+    try:
+        repository.transaction.create(db, t)
+    except:
+        raise HTTPException(status_code=status.HTTP_402_PAYMENT_REQUIRED)
     return t
