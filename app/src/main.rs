cfd1731db1b1592055be86d8152dba61c33206a4
Souma Suzuki <subayai.kobashiri@gmail.com>
2022-06-03 07:20:37 +0000

    feat: axum hello world

---
+++ app/src/main.rs
@@ -0,0 +1,13 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello World" }));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
